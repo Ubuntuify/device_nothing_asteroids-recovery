641bb5226eac35f95ed5aa158a6cd95f56599726
yacha <yacha@orangefox.tech>
2025-05-01 12:21:46 +0200

    Downgrade android.hardware.health

--- device.mk
+++ device.mk
@@ -46,8 +46,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
