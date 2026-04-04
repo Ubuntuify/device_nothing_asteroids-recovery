14b09ba44ff8a0a7ad78ba7831215487092df4f2
Ryan Salazar <ryanconrad2007@gmail.com>
2026-04-05 01:26:35 +0800

    version: update version as there is an important fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,18 @@
 # OrangeFox version options
-# - This should, and can change between releases.
+# This should, and can change between releases.
 export FOX_VARIANT="6.1"
-export FOX_MAINTAINER_PATCH_VERSION=1
+export FOX_MAINTAINER_PATCH_VERSION=2
 
-# OrangeFox options
-# - This should not be changed between releases.
+# OrangeFox Options
+#
+# According to documentation, all build variables that start with FOX should be exported
+# as it will not be applied otherwise. For further reference, refer to the documentation
+# of build variables.
+#
+# This should not be changed between releases unless necessary
 export FOX_TARGET_DEVICES="asteroids,Asteroids"
 
-# dmsetup for wiping /data
-export FOX_USE_DMSETUP=1
-
+export FOX_USE_DMSETUP=1 # dmsetup for wiping /data
 export FOX_VIRTUAL_AB_DEVICE=1
 export FOX_VANILLA_BUILD=1
 export FOX_ENABLE_APP_MANAGER=1
