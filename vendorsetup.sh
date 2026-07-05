Ubuntuify/device_nothing_asteroids-recovery

commit 5969c018098b4af6b3e84e32e299e9162efb5075

encrypt, services: refactor and optimize their running

recovery/root/init.recovery.encryption.rc
@@ -4,7 +4,7 @@
 # Copyright (C) 2026 Ubuntuify
 # SPDX-License-Identifier: GPL-3.0-only
 
-on init
+on property:twrp.firmware.mounted=true 
     start vendor.qseecomd
 
 # Runs after qseecomd runs
@@ -24,15 +24,34 @@ on property:twrp.all.users.decrypted=true
 
 # Runs after decryption
 on property:twrp.all.users.decrypted=true
+    setprop twrp.firmware.mounted false
+
     write /sys/bus/platform/devices/1d84000.ufshc/auto_hibern8 10000
     write /sys/bus/platform/devices/1d84000.ufshc/clkgate_enable 1
     write /sys/bus/platform/devices/1d84000.ufshc/enable_wb_buf_flush 1
 
-# HM: Runs during decryption process
-on property:twrp.firmware.mounted=true
-    exec u:r:recovery:s0 -- /system/bin/sh -c "/system/bin/sleep 1"
+# Runs after decryption (when user has dual apps activated/second space)
+on property:twrp.decrypt.done=true && property:twrp.user.999.decrypt=0
+    setprop twrp.firmware.mounted false
 
+    write /sys/bus/platform/devices/1d84000.ufshc/auto_hibern8 10000
+    write /sys/bus/platform/devices/1d84000.ufshc/clkgate_enable 1
+    write /sys/bus/platform/devices/1d84000.ufshc/enable_wb_buf_flush 1
+
+# Runs if decryption failed/was canceled
+on property:of_decryption_failed=true
+    setprop twrp.firmware.mounted false
+
+    write /sys/bus/platform/devices/1d84000.ufshc/auto_hibern8 10000
+    write /sys/bus/platform/devices/1d84000.ufshc/clkgate_enable 1
+    write /sys/bus/platform/devices/1d84000.ufshc/enable_wb_buf_flush 1
+
+on property:ro.twrp.fastbootd=1 && property:twrp.firmware.mounted=true
+    setprop twrp.firmware.mounted false
+
+# HM: Runs during decryption process
+on property:twrp.firmware.mounted=true && property:twrp.firmware.umounted=
     umount /vendor/firmware_mnt
     umount /mnt/vendor/persist
 
-    setprop twrp.firmware.mounted false
+    setprop twrp.firmware.umounted true

recovery/root/vendor/etc/init/android.hardware.vibrator.service.asteroids.rc
@@ -2,7 +2,7 @@
 # Copyright (C) 2026 Ubuntuify
 # SPDX-License-Identifier: Apache-2.0
 
-on boot
+on property:twrp.modules.loaded=true && property:twrp.firmware.loaded=true 
     wait /dev/aac_richtap
     chown system system /dev/aac_richtap
     chmod 0600 /dev/aac_richtap

recovery/root/init.recovery.qcom.rc
@@ -38,11 +38,6 @@ on early-fs
     write /proc/sys/walt/sched_lib_name "recovery,magiskboot,Updater,updater,update_engine_sideload"
     write /proc/sys/walt/sched_lib_mask_force 255
 
-on property:twrp.modules.loaded=true
-    # Load health hal and battery
-    wait /sys/class/power_supply/battery
-    start health-hal-2-1
-
 on fs
     # All entries in recovery.fstab
     write /sys/block/sda/queue/read_ahead_kb 2048

recovery/root/vendor/etc/init/android.hardware.health-service.qti.rc
@@ -1,5 +1,5 @@
 on fs
-    start vendor.health-service
+    start vendor.health-default
 
 service vendor.health-default /vendor/bin/hw/android.hardware.health-service.qti
     class hal

commit 74a25c2d4595931b6a1ccc4256579fc4efbb2800

encrypt, services: fix regressions introduced in 5969c018

recovery/root/init.recovery.encryption.rc
@@ -1,5 +1,3 @@
-# Adapted from amethyst's OFOX recovery tree.
-#
 # Copyright (C) 2026 chickendrop89
 # Copyright (C) 2026 Ubuntuify
 # SPDX-License-Identifier: GPL-3.0-only
@@ -50,7 +48,7 @@ on property:ro.twrp.fastbootd=1 && property:twrp.firmware.mounted=true
     setprop twrp.firmware.mounted false
 
 # HM: Runs during decryption process
-on property:twrp.firmware.mounted=true && property:twrp.firmware.umounted=
+on property:twrp.firmware.mounted=false && property:twrp.firmware.umounted=
     umount /vendor/firmware_mnt
     umount /mnt/vendor/persist
 

recovery/root/vendor/etc/init/android.hardware.vibrator.service.asteroids.rc
@@ -2,10 +2,13 @@
 # Copyright (C) 2026 Ubuntuify
 # SPDX-License-Identifier: Apache-2.0
 
-on property:twrp.modules.loaded=true && property:twrp.firmware.loaded=true 
+on fs
     wait /dev/aac_richtap
     chown system system /dev/aac_richtap
     chmod 0600 /dev/aac_richtap
+
+on post-fs-data # we need to make sure that this runs after decryption
+    wait /vendor/odm/etc/aac_richtap.config
     start vendor.vibrator
 
 service vendor.vibrator /vendor/bin/hw/android.hardware.vibrator.service.asteroids

sepolicy/file_contexts
@@ -1,1 +1,1 @@
-/dev/ics_sfdc   u:object_r:vibrator_device:s0
+/dev/aac_richtap   u:object_r:vibrator_device:s0

vendorsetup.sh
@@ -43,6 +43,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ];
     export FOX_ENABLE_APP_MANAGER=1
 
     # GKI device (with ported 6.6, or default 6.1)
+    export FOX_ENABLE_KERNELSU_SUPPORT=1
     export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
     export FOX_ENABLE_SUKISU_SUPPORT=1
 